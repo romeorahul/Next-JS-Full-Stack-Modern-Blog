5b35010a1ed8380baa00029452aa100e7fbf2c55
ROMEO <111235095+romeorahul@users.noreply.github.com>
2023-07-28 14:06:15 +0530

    on home page services section added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,6 +3,9 @@ import React from "react";
 import HomeImg from "public/hero.png";
 import styles from "./page.module.css";
 import { db } from "@/utils/db";
+import web from "public/web.svg";
+import android from "public/android.svg";
+import graphic from "public/graphic.svg";
 
 // Rest of your server setup and routes
 
@@ -14,24 +17,79 @@ process.on("SIGINT", () => {
   });
 });
 
-
 const page = () => {
   return (
-    <div className={styles.container}>
-      <div className={styles.texts}>
-        <h1>Welcome to the modern blog</h1>
-        <p>
-          It is a long established fact that a reader will be distracted by the
-          readable content of a page when looking at its layout. The point of
-          using Lorem Ipsum is that it has a more-or-less normal distribution of
-          letters
-        </p>
-        <button className={styles.btn}>Read More</button>
+    <>
+      <div className={styles.container}>
+        <div className={styles.texts}>
+          <h1>Welcome to the modern blog</h1>
+          <p>
+            It is a long established fact that a reader will be distracted by
+            the readable content of a page when looking at its layout. The point
+            of using Lorem Ipsum is that it has a more-or-less normal
+            distribution of letters
+          </p>
+          <button className={styles.btn}>Read More</button>
+        </div>
+        <div className={styles.imgContainer}>
+          <Image src={HomeImg} alt="home page image" className={styles.img} />
+        </div>
       </div>
-      <div className={styles.imgContainer}>
-        <Image src={HomeImg} alt="home page image" className={styles.img} />
+
+      <div className={styles.about}>
+        <h1 className={styles.aboutheading}>Our Services</h1>
+        <div className={styles.box}>
+          <div className={styles.innerbox}>
+            <Image src={web} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+          <div className={styles.innerbox}>
+            <Image src={android} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+          <div className={styles.innerbox}>
+            <Image src={graphic} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+          <div className={styles.innerbox}>
+            <Image src={web} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+          <div className={styles.innerbox}>
+            <Image src={web} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+          <div className={styles.innerbox}>
+            <Image src={web} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+          <div className={styles.innerbox}>
+            <Image src={web} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+          <div className={styles.innerbox}>
+            <Image src={web} className={styles.serviceimg} />
+            <h1>Web development</h1>
+            <p>It is a long established fact that a reader</p>
+            <button className={styles.aboutbtn}>Read More</button>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
